ed5fa773d7acf76887c94262c6387ed64b9d6b1b
progbmy <skuter63@mail.ru>
2019-08-02 13:40:43 +0400

    save vizit information to database

--- app.rb
+++ app.rb
@@ -5,17 +5,17 @@ require 'sqlite3'
 
 
 configure do
-@db = SQLite3::Database.new 'barbershop.db'
-@db.execute 'CREATE TABLE IF NOT EXISTS
+db = get_db
+db.execute 'CREATE TABLE IF NOT EXISTS
 	 "users" 
 	 (
 	  `id` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-	   `username` TEXT,
+	 	`username` TEXT,
 	    `phone` TEXT,
-	     `datestamp` TEXT,
-	      `barber` TEXT,
-	       `color` TEXT
-	        )'	
+	    `datestamp` TEXT,
+	    `barber` TEXT,
+	    `color` TEXT
+	    )'	
 
 end
 
@@ -51,6 +51,20 @@ post '/visit' do
 		return erb :visit
 	end
 
+	db = get_db
+	db.execute 'insert into 
+	Users 
+	(username, 
+	phone, 
+	datestamp, 
+	barber, 
+	color)
+	values (?, ?, ?, ?,?)', [@username, @phone, @datetime, @barber, @color]
+
 	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 
 end
+
+def get_db
+	return SQLite3::Database.new 'barbershop.db'
+end	
